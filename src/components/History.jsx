a860a34a607ba6ba7b50e06688bfeb14d7ace447
Farul Ivan <farulivan@gmail.com>
2023-01-12 13:47:14 +0700

    respond to no history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -5,13 +5,16 @@ const History = () => {
   const history = useSelector(state => state.history);
 
   return (
-    <div className="history">
-      {history.map((item, index) => (
-        <p key={index} className="history__item">
-          {item[0]} = 
-          <span> {item[1]}</span>
-        </p>
-      ))}
+    <div className={history.length > 0 ? "history" : "history center"}>
+      {history.length > 0 ? (
+        history.map((item, index) => (
+          <p key={index} className="history__item">
+            {item[0]} =<span> {item[1]}</span>
+          </p>
+        ))
+      ) : (
+        <p className="history__notfound">You have no calculation history</p>
+      )}
     </div>
   );
 };
